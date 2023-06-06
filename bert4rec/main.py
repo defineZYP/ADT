364c79487d6ce6d47b1889616d47e382152f214d
root <zhang-yp22@mails.tsinghua.edu.cn>
2023-06-06 23:21:52 +0800

    fix bug

--- bert4rec/main.py
+++ bert4rec/main.py
@@ -8,11 +8,10 @@ from utils import *
 
 def main():
     if args.template:
-        args = set_template(args)
-    print(args)
-    export_root = setup_train(args)
-    trainer = BertTrainer(args)
-    lambda1, lambda2 = get_lambda(args.dataset, args.topk)
+        train_args = set_template(args)
+    export_root = setup_train(train_args)
+    trainer = BertTrainer(train_args)
+    lambda1, lambda2 = get_lambda(train_args.dataset, train_args.topk)
     best_epoch, rec_valid, rec_valid_AUC, rec_test, rec_test_AUC = trainer.train(lambda1, lambda2)
     print(f"HERE: {rec_valid[0][5]:.4f}, {rec_valid[1][5]:.4f}, {rec_valid_AUC}, {rec_test[0][5]:.4f}, {rec_test[1][5]:.4f}, {rec_test_AUC})")
     # trainer.test()
